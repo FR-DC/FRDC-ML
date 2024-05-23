d36d95cf3055fe938c7b723e975e8bf3eeee932f
Evening <dev_evening@hotmail.com>
2024-05-23 22:02:13 +0800

    Add fixmatch for efficientnet

--- src/frdc/models/efficientnetb1.py
+++ src/frdc/models/efficientnetb1.py
@@ -9,8 +9,44 @@ from torchvision.models import (
     EfficientNet_B1_Weights,
 )
 
+from frdc.train.fixmatch_module import FixMatchModule
 from frdc.train.mixmatch_module import MixMatchModule
 from frdc.utils.ema import EMA
+
+
+def adapt_efficient_multi_channel(
+    eff: EfficientNet,
+    in_channels: int,
+) -> EfficientNet:
+    """Adapt the EfficientNet model to accept a different number of
+    input channels.
+
+    Notes:
+        This operation is in-place, however will still return the model
+
+    Args:
+        eff: The EfficientNet model
+        in_channels: The number of input channels
+
+    Returns:
+        The adapted EfficientNet model.
+    """
+    old_conv = eff.features[0][0]
+    new_conv = nn.Conv2d(
+        in_channels=in_channels,
+        out_channels=old_conv.out_channels,
+        kernel_size=old_conv.kernel_size,
+        stride=old_conv.stride,
+        padding=old_conv.padding,
+        bias=old_conv.bias,
+    )
+    new_conv.weight.data[:, :3] = old_conv.weight.data
+    new_conv.weight.data[:, 3:] = old_conv.weight.data[:, 1:2].repeat(
+        1, 5, 1, 1
+    )
+    eff.features[0][0] = new_conv
+
+    return eff
 
 
 class EfficientNetB1MixMatchModule(MixMatchModule):
@@ -67,7 +103,7 @@ class EfficientNetB1MixMatchModule(MixMatchModule):
                 param.requires_grad = False
 
         # Adapt the first layer to accept the number of channels
-        self.eff = self.adapt_efficient_multi_channel(self.eff, in_channels)
+        self.eff = adapt_efficient_multi_channel(self.eff, in_channels)
 
         self.fc = nn.Sequential(
             nn.Linear(self.EFF_OUT_DIMS, n_classes),
@@ -84,47 +120,79 @@ class EfficientNetB1MixMatchModule(MixMatchModule):
         self.ema_updater = EMA(model=self, ema_model=self.ema_model)
         self.ema_lr = ema_lr
 
-    @staticmethod
-    def adapt_efficient_multi_channel(
-        eff: EfficientNet,
-        in_channels: int,
-    ) -> EfficientNet:
-        """Adapt the EfficientNet model to accept a different number of
-        input channels.
-
-        Notes:
-            This operation is in-place, however will still return the model
-
-        Args:
-            eff: The EfficientNet model
-            in_channels: The number of input channels
-
-        Returns:
-            The adapted EfficientNet model.
-        """
-        old_conv = eff.features[0][0]
-        new_conv = nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=old_conv.out_channels,
-            kernel_size=old_conv.kernel_size,
-            stride=old_conv.stride,
-            padding=old_conv.padding,
-            bias=old_conv.bias,
-        )
-        new_conv.weight.data[:, :3] = old_conv.weight.data
-        new_conv.weight.data[:, 3:] = old_conv.weight.data[:, 1:2].repeat(
-            1, 5, 1, 1
-        )
-        eff.features[0][0] = new_conv
-
-        return eff
-
     @property
     def ema_model(self):
         return self._ema_model
 
     def update_ema(self):
         self.ema_updater.update(self.ema_lr)
+
+    def forward(self, x: torch.Tensor):
+        """Forward pass."""
+        return self.fc(self.eff(x))
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(
+            self.parameters(), lr=self.lr, weight_decay=self.weight_decay
+        )
+
+
+class EfficientNetB1FixMatchModule(FixMatchModule):
+    MIN_SIZE = 320
+    EFF_OUT_DIMS = 1280
+
+    def __init__(
+        self,
+        *,
+        in_channels: int,
+        n_classes: int,
+        lr: float,
+        x_scaler: StandardScaler,
+        y_encoder: OrdinalEncoder,
+        weight_decay: float = 1e-5,
+        frozen: bool = True,
+    ):
+        """Initialize the EfficientNet model.
+
+        Args:
+            in_channels: The number of input channels.
+            n_classes: The number of classes.
+            lr: The learning rate.
+            x_scaler: The X input StandardScaler.
+            y_encoder: The Y input OrdinalEncoder.
+            weight_decay: The weight decay.
+            frozen: Whether to freeze the base model.
+
+        Notes:
+            - Min input size: 320 x 320
+        """
+        self.lr = lr
+        self.weight_decay = weight_decay
+
+        super().__init__(
+            n_classes=n_classes,
+            x_scaler=x_scaler,
+            y_encoder=y_encoder,
+        )
+
+        self.eff = efficientnet_b1(
+            weights=EfficientNet_B1_Weights.IMAGENET1K_V2
+        )
+
+        # Remove the final layer
+        self.eff.classifier = nn.Identity()
+
+        if frozen:
+            for param in self.eff.parameters():
+                param.requires_grad = False
+
+        # Adapt the first layer to accept the number of channels
+        self.eff = adapt_efficient_multi_channel(self.eff, in_channels)
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.EFF_OUT_DIMS, n_classes),
+            nn.Softmax(dim=1),
+        )
 
     def forward(self, x: torch.Tensor):
         """Forward pass."""
